0172f7d32d9e9686b0a78b3cc8d3be93d566a8e6
Chandra kesh Alapati <103426526+ckedu@users.noreply.github.com>
2022-11-17 19:28:21 -0600

    router controller change

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace clouddev_backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
